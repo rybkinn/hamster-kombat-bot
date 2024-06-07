dbb8ce3a70d70edf1f7581117c9d7b568f0d8979
Nikita Rybkin(win) <4rybkin@gmail.com>
2024-06-07 19:54:16 +0300

    first work version

--- hamster-kombat-bot/__main__.py
+++ hamster-kombat-bot/__main__.py
@@ -1,70 +1,74 @@
 import time
 import os
+import random
+import signal
+import multiprocessing
 
 import pyautogui
+import keyboard
 import cv2
 import numpy as np
 
 
-__VERSION__ = '0.0.1'
+__VERSION__ = '1.0.0'
 
 
-def change() -> None:
-    global work
-    work = not work
-
-
-work = False
-
-
-def find(template_path: str, threshold: float =0.9, interval: float =1) -> None:
+def hook(pid: int, keybind: str) -> None:
     """
-    Находит шаблон изображения на экране и при его обнаружении выполняет указанное действие.
+    Постоянно проверяет, нажата ли конкретная клавиша.
+    Если клавиша нажата, то отправляет сигнал SIGTERM указанному процессу
+    и завершает процесс.
 
-    Args:
-        template_path (str): путь к файлу шаблона изображения.
-        threshold (float, optional): минимальный порог сходства для сопоставления шаблонов. По умолчанию 0,9.
-        interval (float, optional): интервал между каждой проверкой шаблона. По умолчанию 1.
+    Аргументы:
+        pid (int): Идентификатор процесса, которому будет отправлен сигнал SIGTERM.
+        keybind (str): Клавиша, которую нужно проверить.
     """
-
-    # Загружаем изображение, которое мы хотим найти на экране. 0- в градациях серого.
-    template = cv2.imread(template_path, 0)
-    w, h = template.shape[::-1]
-
-    try:
-        while True:
-            if work:
-
-                # Получаем скриншот экрана.
-                screenshot = pyautogui.screenshot()
-                screenshot = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
-
-                # Переводим скриншот в оттенки серого.
-                gray_screenshot = cv2.cvtColor(screenshot, cv2.COLOR_BGR2GRAY)
-
-                # Находим изображение на экране.
-                result = cv2.matchTemplate(gray_screenshot, template, cv2.TM_CCOEFF_NORMED)
-                loc = np.where(result >= threshold)
-
-                for _ in zip(*loc[::-1]):
-                    click(f'img{os.sep}coin.bmp', interval=0.3)
-                    break
-                time.sleep(interval)
-    
-    except KeyboardInterrupt:
-        print('\nВыход из программы')
+    while True:
+        if keyboard.is_pressed(keybind):
+            os.kill(pid, signal.SIGTERM)
+            os._exit(1)
 
 
-def click(template_path: str, threshold: float =0.8, interval: float =0.0001) -> None:
+def find(template: str, threshold: float =0.9) -> tuple:
     """
-    Кликает по изображению на экране на основе шаблонного изображения.
+    Находит расположение изображения-шаблона на экране.
 
-    Args:
-        template_path (str): Путь к шаблонному изображению.
-        threshold (float, optional): Минимальный порог сходства для сопоставления шаблонов. По умолчанию 0.8.
-        interval (float, optional): Интервал между каждым кликом. По умолчанию 0.0001.
+    Аргументы:
+        template (str): Путь к изображению-шаблону.
+        threshold (float, optional): Пороговое значение для сравнения изображения-шаблона. Значение по умолчанию равно 0.9.
 
-    """ 
+    Возвращает:
+        tuple: Кортеж, содержащий координаты совпавшего изображения-шаблона.
+    """
+    # Получаем скриншот экрана.
+    screenshot = pyautogui.screenshot()
+    screenshot = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
+
+    # Переводим скриншот в оттенки серого.
+    gray_screenshot = cv2.cvtColor(screenshot, cv2.COLOR_BGR2GRAY)
+
+    # Настрой изображение на экране.
+    result = cv2.matchTemplate(gray_screenshot, template, cv2.TM_CCOEFF_NORMED)
+    loc = np.where(result >= threshold)
+    return loc
+
+
+def click(template_path: str, tap_count: int, threshold: float =0.8) -> None:
+    """
+    Функция кликает по изображению на экране на основе заданного шаблона.
+
+    Аргументы:
+        template_path (str): Путь к изображению шаблона.
+        tap_count (int): Количество кликов по изображению.
+        threshold (float, optional): Порог соответствия изображению шаблону. Значение по умолчанию - 0.8.
+
+    Эта функция принимает шаблон изображения, считывает его и преобразует в оттенки серого.
+    Затем она делает скриншот и преобразует его в оттенки серого. Используется соответствие
+    шаблонов для поиска расположения изображения на экране. Если изображение найдено, 
+    то происходит клик по центру изображения с случайными смещениями. 
+    Количество кликов определяется параметром tap_count. Функция выводит количество оставшихся 
+    кликов для каждого клика.
+    """
     template = cv2.imread(template_path, 0)
     w, h = template.shape[::-1]
 
@@ -82,10 +86,50 @@ def click(template_path: str, threshold: float =0.8, interval: float =0.0001) ->
     for pt in zip(*loc[::-1]):
         center_x = pt[0] + w // 2
         center_y = pt[1] + h // 2
-        for _ in range(260):
-            pyautogui.doubleClick(center_x, center_y, interval=interval)
+        taps_left = tap_count
+        for _ in range(tap_count):
+            offset_x = random.randint(-50, 50)
+            offset_y = random.randint(-50, 50)
+            x = center_x + offset_x
+            y = center_y + offset_y
+            interval = random.uniform(0.001, 0.3)
+            pyautogui.click(x, y, interval=interval)
+            taps_left -= 1
+            print(f"{taps_left}/{tap_count}")
         break
 
 
 if __name__ == "__main__":
-    find(f'img{os.sep}energy.bmp')
+
+    ENERGY = 5500
+    ONE_TAP_ENERGY = 11
+    
+    # Ждем, пока пользователь нажмет клавишу запуска бота
+    keybind_start = "`"
+    print(f"Нажмите '{keybind_start}' для запуска бота")
+    keyboard.wait(keybind_start)
+
+    # Мониторим завершение работы бота по нажатию клавиши в отдельном процессе
+    pid = os.getpid()
+    keybind_exit = "-"
+    multiprocessing.Process(target=hook, args=(pid, keybind_exit)).start()
+
+    # Запускаем бота
+    print("Бот запущен")
+
+    # Загружаем изображение, которое мы хотим найти на экране. 0- в градациях серого.
+    template = cv2.imread(f'..{os.sep}img{os.sep}energy.bmp', 0)
+
+    while True:
+        loc = find(template)
+
+        for _ in zip(*loc[::-1]):
+            offset_tap_count = random.randint(-100, 0)
+            tap_count = ENERGY // ONE_TAP_ENERGY + offset_tap_count
+            print(f"Tap count: {tap_count}")
+            click(f'..{os.sep}img{os.sep}coin.bmp', tap_count)
+            break
+
+        print("Ожидание пополнения энергии")
+        time.sleep(1500) # 25min
+        print("Пополнение энергии завершено")
